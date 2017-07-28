e2b8da71d24a52b64273cd293312a14439185ed2
Ricardo Valencia Quintero <ricardovalquin@gmail.com>
2017-07-28 14:53:44 -0500

    Created the reduced for the weather service

---
+++ src/reducers/Reducer-weather.js
@@ -0,0 +1,4 @@
+// always the first argument is the state, defaulted to null, the second one is the action
+export default function(state = null, action) {
+  return state;
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import WeatherReducer from './Reducer-weather';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
